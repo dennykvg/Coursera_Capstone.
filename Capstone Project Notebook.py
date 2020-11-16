933850de6f25897cb089ba1d9fcafb6846072080
dennykvg <69587852+dennykvg@users.noreply.github.com>
2020-11-16 17:45:50 +0400

    Add files via upload

---
+++ Capstone Project Notebook.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Cap Stone Project
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
